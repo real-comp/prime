178b89962bebf929137beaa93647b7c078fc2f3e
Kyle Renfro <krenfro@real-comp.com>
2012-02-02 16:34:36 +0000

    Worked out some kinks in the entryset handling of Record.  Maps are treated as leaf nodes only if they are empty.
    git-svn-id: svn+ssh://dev.real-comp.com/opt/usr/share/rcsvn/realcomp-data/trunk@3749 cb9be8fc-bb24-4f4f-8dc9-9fc556ea96d8

--- src/main/java/com/realcomp/data/record/RecordEntries.java
+++ src/main/java/com/realcomp/data/record/RecordEntries.java
@@ -7,59 +7,62 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.logging.Logger;
 
 /**
- * Returns all (leaf) entries in a Record.  The order of the entries is not defined. 
- * 
- * 
+ * Returns all (leaf) entries in a Record. The order of the entries is not defined.
+ *
+ *
  * @author krenfro
  */
 class RecordEntries {
-    
-     static Set<Map.Entry<String,Object>> getEntries(Map<String,Object> map){
-        
-        Set<Map.Entry<String,Object>> entries = new HashSet<Map.Entry<String,Object>>(); 
-        
-        for (Map.Entry<String,Object> entry: map.entrySet()){            
+
+    private static final Logger logger = Logger.getLogger(RecordEntries.class.getName());
+
+    static Set<Map.Entry<String, Object>> getEntries(Map<String, Object> map) {
+
+        Set<Map.Entry<String, Object>> entries = new HashSet<Map.Entry<String, Object>>();
+
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
             String fieldName = entry.getKey();
             Object value = entry.getValue();
-            if (isLeaf(value)){
+            if (isLeaf(value)) {
                 entries.add(new AbstractMap.SimpleEntry(fieldName, value));
             }
-            else{
+            else {
                 DataType type = DataType.getDataType(value);
-                if (type == DataType.LIST){
+                if (type == DataType.LIST) {
                     entries.addAll(getEntries((List) value, fieldName));
                 }
-                else if (type == DataType.MAP){
+                else if (type == DataType.MAP) {
                     entries.addAll(getEntries((Map) value, fieldName + "."));
                 }
             }
         }
-        
+
         return entries;
     }
-     
-    private static Set<Map.Entry<String,Object>> getEntries(List list, String prefix){
-        
-        assert(!isLeaf(list));
-        
-        Set<Map.Entry<String,Object>> entries = new HashSet<Map.Entry<String,Object>>();        
-        if (list != null){
-            for (int index = 0; index < list.size(); index++){                
+
+    private static Set<Map.Entry<String, Object>> getEntries(List list, String prefix) {
+
+        assert (!isLeaf(list));
+
+        Set<Map.Entry<String, Object>> entries = new HashSet<Map.Entry<String, Object>>();
+        if (list != null) {
+            for (int index = 0; index < list.size(); index++) {
                 Object value = list.get(index);
-                if (isLeaf(value)){
+                if (isLeaf(value)) {
                     entries.add(new AbstractMap.SimpleEntry(String.format("%s[%s]", prefix, index), value));
                 }
-                else{
+                else {
                     DataType type = DataType.getDataType(value);
-                    if (type == DataType.LIST){
-                        Set<Map.Entry<String,Object>> temp = 
+                    if (type == DataType.LIST) {
+                        Set<Map.Entry<String, Object>> temp =
                                 getEntries((List) value, String.format("%s[%s]", prefix, index));
                         entries.addAll(temp);
                     }
-                    else if (type == DataType.MAP){
-                        Set<Map.Entry<String,Object>> temp = 
+                    else if (type == DataType.MAP) {
+                        Set<Map.Entry<String, Object>> temp =
                                 getEntries((Map) value, String.format("%s[%s].", prefix, index));
                         entries.addAll(temp);
                     }
@@ -68,66 +71,66 @@ class RecordEntries {
         }
         return entries;
     }
-    
-    /**
-     * Determines if the value represents a leaf value in the record.
-     * Non-Leaf values are maps and lists that contain maps or lists.
-     * @param value
-     * @return 
-     */
-    private static boolean isLeaf(Object value){
-        
-        boolean leaf = true;
-        if (value != null){
-            DataType type = DataType.getDataType(value);
-            if (type == DataType.LIST){
-                for (Object entry: (List) value){
-                    if (entry != null){
-                        DataType entryType = DataType.getDataType(entry);
-                        if (entryType == DataType.LIST || entryType == DataType.MAP){
-                            leaf = false;
-                            break;
-                        }
-                    }
-                }
-            }
-            else if (type == DataType.MAP){
-                for (Entry<String,Object> entry: ((Map<String,Object>)value).entrySet()){
-                    if (entry != null){
-                        DataType entryType = DataType.getDataType(entry.getValue());
-                        if (entryType == DataType.LIST || entryType == DataType.MAP){
-                            leaf = false;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        return leaf;
-    }
-    
-    
-    private static Set<Map.Entry<String,Object>> getEntries(Map<String,Object> map, String prefix){
-        
-        Set<Map.Entry<String,Object>> entries = new HashSet<Map.Entry<String,Object>>(); 
-        
-        for (Map.Entry<String,Object> entry: map.entrySet()){            
+
+    private static Set<Map.Entry<String, Object>> getEntries(Map<String, Object> map, String prefix) {
+
+        Set<Map.Entry<String, Object>> entries = new HashSet<Map.Entry<String, Object>>();
+
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
             String fieldName = prefix.concat(entry.getKey());
             Object value = entry.getValue();
-            if (isLeaf(value)){
+            if (isLeaf(value)) {
                 entries.add(new AbstractMap.SimpleEntry(fieldName, value));
             }
-            else{
+            else {
                 DataType type = DataType.getDataType(value);
-                if (type == DataType.LIST){
+                if (type == DataType.LIST) {
                     entries.addAll(getEntries((List) value, fieldName));
                 }
-                else if (type == DataType.MAP){
+                else if (type == DataType.MAP) {
                     entries.addAll(getEntries((Map) value, fieldName + "."));
                 }
             }
         }
-        
+
         return entries;
-    }    
+    }
+
+    /**
+     * Determines if the value represents a leaf value in the record. 
+     * <p> 
+     *  <b>Non</b>-Leaf values are: 
+     *  <ul> 
+     *   <li>non-empty maps</li> 
+     *   <li>lists that contain maps or lists</li>
+     *  </ul>
+     * </p>
+     *
+     * @param value
+     * @return
+     */
+    private static boolean isLeaf(Object value) {
+
+        boolean leaf = true;
+        if (value != null) {
+            DataType type = DataType.getDataType(value);
+            if (type == DataType.LIST) {
+                for (Object entry : (List) value) {
+                    if (entry != null) {
+                        DataType entryType = DataType.getDataType(entry);
+                        if (entryType == DataType.LIST || entryType == DataType.MAP) {
+                            leaf = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            else if (type == DataType.MAP) {
+                leaf = ((Map) value).isEmpty();
+            }
+        }
+        return leaf;
+    }
+
+    
 }
