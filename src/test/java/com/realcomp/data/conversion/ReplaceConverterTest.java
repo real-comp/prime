2dde82868e6bbd76ca1f1205998d8844b3106bd9
Kyle Renfro <krenfro@real-comp.com>
2012-10-25 18:03:00 +0000

    git-svn-id: svn+ssh://dev.real-comp.com/opt/usr/share/rcsvn/realcomp-data/trunk@5515 cb9be8fc-bb24-4f4f-8dc9-9fc556ea96d8

--- src/test/java/com/realcomp/data/conversion/ReplaceConverterTest.java
+++ src/test/java/com/realcomp/data/conversion/ReplaceConverterTest.java
@@ -20,12 +20,12 @@ public class ReplaceConverterTest extends SimpleConverterTest{
     @Test
     public void testConvert() throws Exception {
 
-        Replace converter = new Replace();        
-        
+        Replace converter = new Replace();
+
         assertEquals("", converter.getRegex());
         assertEquals("", converter.getReplacement());
-        
-        
+
+
         converter.setRegex("a");
         assertEquals("", converter.convert("aaa"));
         assertEquals("b", converter.convert("aab"));
@@ -33,14 +33,14 @@ public class ReplaceConverterTest extends SimpleConverterTest{
         assertEquals("b", converter.convert("baa"));
         assertEquals("bb", converter.convert("bab"));
     }
-    
+
     @Test
     public void testRemoveMultiSpaces() throws Exception{
-        
+
         Replace converter = new Replace();
         converter.setRegex("  ");
         converter.setReplacement(" ");
-        
+
         assertEquals(" ", converter.convert("  "));
         assertEquals(" a", converter.convert("  a"));
         assertEquals(" a ", converter.convert("  a "));
@@ -54,15 +54,15 @@ public class ReplaceConverterTest extends SimpleConverterTest{
         assertEquals("a a ", converter.convert("a     a   "));
         assertEquals("a a ", converter.convert("a     a    "));
     }
-    
-   
+
+
     @Test
     public void testLeadingZeros() throws Exception {
 
-        Replace replace = new Replace();        
+        Replace replace = new Replace();
         replace.setRegex("^0");
         replace.setReplacement("");
-        
+
         assertEquals("1", replace.convert("1"));
         assertEquals("1", replace.convert("01"));
         assertEquals("1", replace.convert("001"));
@@ -70,41 +70,55 @@ public class ReplaceConverterTest extends SimpleConverterTest{
         assertEquals("1", replace.convert("00001"));
     }
 
-   
-   
+
+
     @Test
     public void testCopyOf() {
         Replace a = new Replace();
         Replace b = a.copyOf();
         assertEquals(a, b);
     }
-    
+
     @Test
     public void testHashCode() {
         Replace a = new Replace();
         Replace b = new Replace();
         assertEquals(a.hashCode(), b.hashCode());
     }
-    
+
     @Test
     @Override
     public void testEquals() {
-        
+
         super.testEquals();
-        
+
         Replace a = new Replace();
         Replace b = new Replace();
         assertEquals(a, b);
         b.setRegex("A");
         assertFalse(a.equals(b));
         b.setRegex(a.getRegex());
-        
+
         assertEquals(a, b);
         b.setReplacement("A");
         assertFalse(a.equals(b));
         b.setReplacement(a.getReplacement());
-        
+
     }
-    
-    
+
+
+    @Test
+    public void testAsterisks() throws Exception {
+
+        Replace converter = new Replace();
+        converter.setRegex("[\\*].+");
+        converter.setReplacement("*");
+
+        assertEquals("123", converter.convert("123"));
+        assertEquals("123*", converter.convert("123*"));
+        assertEquals("123*", converter.convert("123*a"));
+    }
+
+
+
 }
