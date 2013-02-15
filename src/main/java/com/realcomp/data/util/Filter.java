d4e0188fb9d107b088da6821a1ce4ca281ba0422
Kyle Renfro <krenfro@real-comp.com>
2013-02-15 22:35:22 +0000

    removed recursive loop in filter.getNextRecord()
    When filtering files with many records filtered, I was running out of stack space.
    
    git-svn-id: svn+ssh://dev.real-comp.com/opt/usr/share/rcsvn/realcomp-data/trunk@6311 cb9be8fc-bb24-4f4f-8dc9-9fc556ea96d8

--- src/main/java/com/realcomp/data/util/Filter.java
+++ src/main/java/com/realcomp/data/util/Filter.java
@@ -17,8 +17,8 @@ import joptsimple.OptionSet;
 
 /**
  * WARNING: This class does more than filter, it filters and runs the conversions!
- * 
- * 
+ *
+ *
  * @author krenfro
  */
 public class Filter {
@@ -26,22 +26,22 @@ public class Filter {
     private static final Logger logger =  Logger.getLogger(Filter.class.getName());
 
 
-    public void filter(IOContext in, IOContext out) 
+    public void filter(IOContext in, IOContext out)
             throws SchemaException, IOException, ConversionException, ValidationException {
 
         RecordReader reader = RecordReaderFactory.build(in.getSchema());
         reader.open(in);
         RecordWriter writer = RecordWriterFactory.build(out.getSchema());
-        writer.open(out);    
-        
+        writer.open(out);
+
         Record record = getNextRecord(reader);
         while (record != null){
             try{
                 writer.write(record);
             }
             catch(ValidationException ex){
-                logger.log(Level.INFO, 
-                           "filtered: {0} because: {1}", 
+                logger.log(Level.INFO,
+                           "filtered: {0} because: {1}",
                            new Object[]{out.getSchema().classify(record).toString(record), ex.getMessage()});
             }
             record = getNextRecord(reader);
@@ -54,18 +54,26 @@ public class Filter {
     protected Record getNextRecord(RecordReader reader)
             throws IOException, SchemaException, ConversionException{
 
-        try {
-            return reader.read();
+        Record record = null;
+        boolean done = false;
+        while (!done && record == null){
+            try {
+                record = reader.read();
+                if (record == null){
+                    done = true;
+                }
+            }
+            catch (ValidationException ex) {
+                logger.log(Level.INFO,
+                           "filtered input record because: {0}",
+                           new Object[]{ex.getMessage()});
+
+                record = null;
+            }
         }
-        catch (ValidationException ex) {
-            logger.log(Level.INFO, 
-                       "filtered input record because: {0}", 
-                       new Object[]{ex.getMessage()});
-            
-            return getNextRecord(reader);
-        }
+        return record;
     }
-    
+
     private static void printHelp(OptionParser parser){
         try {
             parser.printHelpOn(System.err);
@@ -73,49 +81,49 @@ public class Filter {
         catch (IOException ignored) {
         }
     }
-    
+
 
     public static void main(String[] args){
 
         OptionParser parser = new OptionParser(){{
             acceptsAll(Arrays.asList("is","input-schema"), "input schema" )
                     .withRequiredArg().describedAs("schema").required();
-            
+
             acceptsAll(Arrays.asList("os", "output-schema"), "output schema" )
                     .withRequiredArg().describedAs("schema").required();
-            
+
             accepts("in", "input file (default: STDIN)").withRequiredArg().describedAs("file");
             accepts("out", "output file (default: STDOUT)").withRequiredArg().describedAs("file");
             acceptsAll(Arrays.asList("h", "?", "help"), "help");
         }};
-        
+
         int result = 1;
-        
+
         try{
             OptionSet options = parser.parse(args);
             if (options.has("?")){
                 printHelp(parser);
             }
             else{
-                Filter filterer = new Filter();       
-                IOContextBuilder inputBuilder = new IOContextBuilder();                
+                Filter filterer = new Filter();
+                IOContextBuilder inputBuilder = new IOContextBuilder();
                 inputBuilder.schema(
                         SchemaFactory.buildSchema(new FileInputStream((String) options.valueOf("is"))));
                 inputBuilder.in(
-                        options.has("in") ? 
-                            new BufferedInputStream(new FileInputStream((String) options.valueOf("in"))) : 
+                        options.has("in") ?
+                            new BufferedInputStream(new FileInputStream((String) options.valueOf("in"))) :
                             new BufferedInputStream(System.in));
                 inputBuilder.validationExceptionThreshold(Severity.MEDIUM);
-                
-                IOContextBuilder outputBuilder = new IOContextBuilder();                
+
+                IOContextBuilder outputBuilder = new IOContextBuilder();
                 outputBuilder.schema(
                         SchemaFactory.buildSchema(new FileInputStream((String) options.valueOf("os"))));
                 outputBuilder.out(
-                        options.has("out") ? 
+                        options.has("out") ?
                             new BufferedOutputStream(new FileOutputStream((String) options.valueOf("out"))) :
                             new BufferedOutputStream(System.out));
                 outputBuilder.validationExceptionThreshold(Severity.MEDIUM);
-               
+
                 filterer.filter(inputBuilder.build(), outputBuilder.build());
                 result = 0;
             }
@@ -139,7 +147,7 @@ public class Filter {
             System.err.println(ex.getMessage());
             printHelp(parser);
         }
-        
+
         System.exit(result);
     }
 }
