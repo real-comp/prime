23e04ea7b2c2caf5ae6f3384b8274988318f39ec
Kyle Renfro <krenfro@real-comp.com>
2013-05-28 00:23:13 +0000

    Updated equals/hashcode to fix a findbugs suspicious reference comparison.
    git-svn-id: svn+ssh://dev.real-comp.com/opt/usr/share/rcsvn/realcomp-data/trunk@6773 cb9be8fc-bb24-4f4f-8dc9-9fc556ea96d8

--- src/main/java/com/realcomp/data/conversion/Substring.java
+++ src/main/java/com/realcomp/data/conversion/Substring.java
@@ -1,10 +1,12 @@
 package com.realcomp.data.conversion;
 
+import java.util.Objects;
+
 
 /**
  * behaves like String.substring(begin, end)
  * Bad index will return empty-string.
- * 
+ *
  * @author krenfro
  */
 @com.realcomp.data.annotation.Converter("substring")
@@ -12,34 +14,34 @@ public class Substring extends SimpleConverter {
 
     protected Integer begin = 0;
     protected Integer end;
-    
+
     public Substring(){
         super();
     }
-    
+
     public Substring(Integer begin){
         super();
         this.begin = begin;
     }
-    
+
     public Substring(Integer begin, Integer end){
         super();
         this.begin = begin;
         this.end = end;
     }
-    
+
     @Override
     public Object convert(Object value) throws ConversionException{
-        
+
         if (value == null)
             throw new IllegalArgumentException("value is null");
 
         Object retVal = "";
-        
+
         try{
             if (begin == null)
                 begin = 0;
-            
+
             if (end == null || end <= begin)
                 retVal = value.toString().substring(begin);
             else
@@ -73,24 +75,30 @@ public class Substring extends SimpleConverter {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        final Substring other = (Substring) obj;
-        if (this.begin != other.begin && (this.begin == null || !this.begin.equals(other.begin)))
-            return false;
-        if (this.end != other.end && (this.end == null || !this.end.equals(other.end)))
-            return false;
-        return true;
+    public int hashCode(){
+        int hash = 3;
+        hash = 37 * hash + Objects.hashCode(this.begin);
+        hash = 37 * hash + Objects.hashCode(this.end);
+        return hash;
     }
 
     @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 97 * hash + (this.begin != null ? this.begin.hashCode() : 0);
-        hash = 97 * hash + (this.end != null ? this.end.hashCode() : 0);
-        return hash;
+    public boolean equals(Object obj){
+        if (obj == null){
+            return false;
+        }
+        if (getClass() != obj.getClass()){
+            return false;
+        }
+        final Substring other = (Substring) obj;
+        if (!Objects.equals(this.begin, other.begin)){
+            return false;
+        }
+        if (!Objects.equals(this.end, other.end)){
+            return false;
+        }
+        return true;
     }
+
+    
 }
