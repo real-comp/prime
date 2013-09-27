99fb3988042c2804b8e5d0bd5d78c73783692d92
Kyle Renfro <krenfro@real-comp.com>
2013-09-27 17:53:37 +0000

    git-svn-id: svn+ssh://dev.real-comp.com/opt/usr/share/rcsvn/realcomp-data/trunk@8773 cb9be8fc-bb24-4f4f-8dc9-9fc556ea96d8

--- src/main/java/com/realcomp/data/schema/SchemaFactory.java
+++ src/main/java/com/realcomp/data/schema/SchemaFactory.java
@@ -11,6 +11,16 @@ import java.io.InputStream;
  */
 public class SchemaFactory{
 
+
+    public static Schema buildSchema(String xml){
+
+        if (xml == null){
+            throw new IllegalArgumentException("xml is null");
+        }
+        XStream xstream = XStreamFactory.build();
+        return (Schema) xstream.fromXML(xml);
+    }
+
     public static Schema buildSchema(InputStream in){
 
         if (in == null){
@@ -20,12 +30,31 @@ public class SchemaFactory{
         return (Schema) xstream.fromXML(in);
     }
 
+
+    public static Transformer buildTransformer(String xml){
+        if (xml == null){
+            throw new IllegalArgumentException("xml is null");
+        }
+        XStream xstream = XStreamFactory.build();
+        return (Transformer) xstream.fromXML(xml);
+    }
+
+
     public static Transformer buildTransformer(InputStream in){
         if (in == null){
             throw new IllegalArgumentException("inputStream is null");
         }
         XStream xstream = XStreamFactory.build();
         return (Transformer) xstream.fromXML(in);
+    }
+
+    public static RelationalSchema buildRelationalSchema(String xml){
+
+        if (xml == null){
+            throw new IllegalArgumentException("xml is null");
+        }
+        XStream xstream = XStreamFactory.build();
+        return (RelationalSchema) xstream.fromXML(xml);
     }
 
     public static RelationalSchema buildRelationalSchema(InputStream in){
