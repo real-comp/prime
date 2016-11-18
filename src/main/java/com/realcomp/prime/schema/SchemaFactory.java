72d63531d9a318f8b7d3c6ca5b6136e196e2b5aa
Kyle Renfro <krenfro@users.noreply.github.com>
2016-11-18 13:35:34 -0600

    Fixed bug reading Schemas from InputStream. Using IOUtils to read the stream fully to a String, then creating Schema from that

--- src/main/java/com/realcomp/prime/schema/SchemaFactory.java
+++ src/main/java/com/realcomp/prime/schema/SchemaFactory.java
@@ -3,62 +3,65 @@ package com.realcomp.prime.schema;
 import com.realcomp.prime.schema.xml.XStreamFactory;
 import com.realcomp.prime.transform.Transformer;
 import com.thoughtworks.xstream.XStream;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Objects;
 
 public class SchemaFactory{
 
 
     public static Schema buildSchema(String xml){
-
-        if (xml == null){
-            throw new IllegalArgumentException("xml is null");
-        }
+        Objects.requireNonNull(xml);
         XStream xstream = XStreamFactory.build();
         return new Schema((Schema) xstream.fromXML(xml));
     }
 
     public static Schema buildSchema(InputStream in){
-
-        if (in == null){
-            throw new IllegalArgumentException("InputStream is null");
+        Objects.requireNonNull(in);
+        try{
+            String xml = IOUtils.toString(in);
+            return buildSchema(xml);
         }
-        XStream xstream = XStreamFactory.build();
-        return new Schema((Schema) xstream.fromXML(in));
+        catch (IOException e){
+            throw new IllegalStateException(e);
+        }
     }
 
 
     public static Transformer buildTransformer(String xml){
-        if (xml == null){
-            throw new IllegalArgumentException("xml is null");
-        }
+        Objects.requireNonNull(xml);
         XStream xstream = XStreamFactory.build();
         return (Transformer) xstream.fromXML(xml);
     }
 
 
     public static Transformer buildTransformer(InputStream in){
-        if (in == null){
-            throw new IllegalArgumentException("inputStream is null");
+        Objects.requireNonNull(in);
+        try{
+            String xml = IOUtils.toString(in);
+            return buildTransformer(xml);
         }
-        XStream xstream = XStreamFactory.build();
-        return (Transformer) xstream.fromXML(in);
+        catch (IOException e){
+            throw new IllegalStateException(e);
+        }
     }
 
     public static RelationalSchema buildRelationalSchema(String xml){
-
-        if (xml == null){
-            throw new IllegalArgumentException("xml is null");
-        }
+        Objects.requireNonNull(xml);
         XStream xstream = XStreamFactory.build();
         return (RelationalSchema) xstream.fromXML(xml);
     }
 
     public static RelationalSchema buildRelationalSchema(InputStream in){
-
-        if (in == null){
-            throw new IllegalArgumentException("InputStream is null");
+        Objects.requireNonNull(in);
+        try{
+            String xml = IOUtils.toString(in);
+            return buildRelationalSchema(xml);
         }
-        XStream xstream = XStreamFactory.build();
-        return (RelationalSchema) xstream.fromXML(in);
+        catch (IOException e){
+            throw new IllegalStateException(e);
+        }
     }
 }
